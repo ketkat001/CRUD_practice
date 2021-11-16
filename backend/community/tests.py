16c8b6b4e3a6f0cd41ae047b3b91ae5264f02215
ketkat001 <ketkat001@gmail.com>
2021-11-17 02:05:27 +0900

    backend-005|update community test code

--- backend/community/tests.py
+++ backend/community/tests.py
@@ -16,14 +16,47 @@ class CreateTest(TestCase):
             last_name="user")
         self.user.save()
     
+
     def tearDown(self):
         self.user.delete()
     
+
     def test_create_article(self):
         data = {
             'title': 'test_title',
             'content': 'test_success',
             'user': self.user.id,
         }
-        response= self.client.post('/community/', json.dumps(data), content_type = 'application/json')
+        response = self.client.post(
+            '/community/', 
+            json.dumps(data), 
+            content_type = 'application/json')
+        self.assertEqual(response.status_code, 200)
+
+
+class ReadTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="test_user", 
+            password="1q2w3e4r!!",
+            email="test@naver.com",
+            first_name="test",
+            last_name="user")
+        self.user.save()
+        self.article = Article.objects.create(
+            title="test_title",
+            content="test_content",
+            user=self.user
+        )
+        self.article.save()
+    
+    
+    def test_read_article_list(self):
+        response = self.client.get('/community/')
+        self.assertEqual(response.status_code, 200)
+    
+
+    def test_read_article_detail(self):
+        article = Article.objects.get()
+        response = self.client.get('/community/{{ article.pk }}/')
         self.assertEqual(response.status_code, 200)
