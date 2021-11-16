3d03ae0bb079965fb755523268310a8887f0e407
ketkat001 <ketkat001@gmail.com>
2021-11-16 21:39:27 +0900

    backend-003|create account unit-test for signup

--- backend/account/tests.py
+++ backend/account/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import User
 
+import json
 # Create your tests here.
+
+client = Client()
+
+class SignupTest(TestCase):
+    def test_signup(self):
+        data = {
+            'password': '1q2w3e4r!!',
+            'username': 'ketkat',
+            'first_name': 'YONGJOON',
+            'last_name': 'KANG',
+            'email': 'ketkat001@naver.com',
+        }
+
+        response = client.post('account/signup', json.dumps(data), content_type = 'application/json')
+
+        self.assertEqual(response.status_code, 200)
