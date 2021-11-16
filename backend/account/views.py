08a393a6b9b3d252afd5d9f6b5ae5201a75fb56c
ketkat001 <ketkat001@gmail.com>
2021-11-16 22:33:51 +0900

    backend-004|Create signup function

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from .serializers import CreateUserSerializer, UserSerializer
+from .models import User
 
 # Create your views here.
+
+class RegistrationAPI(generics.GenericAPIView):
+    serializer_class = CreateUserSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response (
+            {
+                'user' : UserSerializer(
+                    user, context = self.get_serializer_context()
+                ).data,
+            }
+        )
