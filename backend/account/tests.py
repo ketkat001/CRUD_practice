93c7979c410cc49d7cce2cf8b3b612d262cf81ff
ketkat001 <ketkat001@gmail.com>
2021-11-16 23:28:22 +0900

    backend-003|add login-test function

--- backend/account/tests.py
+++ backend/account/tests.py
@@ -1,10 +1,9 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from .models import User
 
 import json
 # Create your tests here.
 
-client = Client()
 
 class SignupTest(TestCase):
     def test_signup(self):
@@ -15,5 +14,27 @@ class SignupTest(TestCase):
             'last_name': 'KANG',
             'email': 'ketkat001@naver.com',
         }
-        response = client.post('/account/signup/', json.dumps(data), content_type = 'application/json')
+        response = self.client.post('/account/signup/', json.dumps(data), content_type = 'application/json')
+        self.assertEqual(response.status_code, 200)
+
+
+class LoginTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="test_user", 
+            password="1q2w3e4r!!",
+            email="test@naver.com",
+            first_name="test",
+            last_name="user")
+        self.user.save()
+    
+    def tearDown(self):
+        self.user.delete()
+
+    def test_login(self):
+        data = {
+            'password': '1q2w3e4r!!',
+            'username': 'ketkat',
+        }
+        response= self.client.post('/account/login/', json.dumps(data), content_type = 'application/json')
         self.assertEqual(response.status_code, 200)
